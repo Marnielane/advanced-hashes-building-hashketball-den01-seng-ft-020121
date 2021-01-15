02dd5de3ff4dc56a52c80bb89372bf1e0dbf04f9
Marnie Robinson <marnielane@gmail.com>
2021-01-15 20:52:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -81,24 +81,26 @@ def game_hash
           :slam_dunks => 10 
         },        
         "DeSagna Diop" => {
-          :number => ,
-          :shoe => ,
-          :points => ,
-          :rebounds => ,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks => 
-        },        :name => {
-          :number => ,
-          :shoe => ,
-          :points => ,
-          :rebounds => ,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks => 
-        },        :name => {
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
+        },        
+        "Ben Gordon" => {
+          :number => 8,
+          :shoe => 15,
+          :points => 33,
+          :rebounds => 3,
+          :assists => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0 
+        },        
+        "Kemba Walker" => {
           :number => ,
           :shoe => ,
           :points => ,
