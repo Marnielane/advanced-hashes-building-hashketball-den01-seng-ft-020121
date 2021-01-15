aa97240958ba6bed87b007d7f025b57f6ba32e8f
Marnie Robinson <marnielane@gmail.com>
2021-01-15 20:31:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,17 +5,16 @@ def game_hash
       :colors => ["Black", "White"],
       :players => [
         "Alan Anderson" => {
-          :number => ,
-          :shoe => ,
-          :points => ,
-          :rebounds => ,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks => 
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1  
         },
-        :players => [
-        :name => {
+        "Reggie Evans" => {
           :number => ,
           :shoe => ,
           :points => ,
